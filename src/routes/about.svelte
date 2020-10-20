9aa869b8a5b6ae10e78f7c37457bd5c5a5ab6810
Héctor René <bautista.exobchix@gmail.com>
2020-10-20 02:04:33 -0500

    Integrate Sapper

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis officia impedit esse aut incidunt natus inventore ex neque aliquid eius quidem libero, tenetur fugit accusantium nisi saepe. Commodi, ea molestiae?Dolor facilis nisi asperiores rem dignissimos, perferendis quod, accusamus similique, harum reiciendis rerum sint. Suscipit voluptates, dignissimos nam officiis saepe quibusdam accusantium quae, sit eligendi rerum assumenda iure provident dolorum!</p>
